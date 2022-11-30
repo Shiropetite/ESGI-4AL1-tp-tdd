d5765e94dcbb86f48ac142df3f10a381b9f22ad4
Shiropetite <59747311+Shiropetite@users.noreply.github.com>
2022-11-30 11:18:49 +0100

    RED DrivingLicenceFinderService
    Co-Authored-By: Radalls <29651124+Radalls@users.noreply.github.com>

--- src/test/java/fr/esgi/cleancode/service/DrivingLicenceFinderServiceTest.java
+++ src/test/java/fr/esgi/cleancode/service/DrivingLicenceFinderServiceTest.java
@@ -1,11 +1,19 @@
 package fr.esgi.cleancode.service;
 
 import fr.esgi.cleancode.database.InMemoryDatabase;
+import fr.esgi.cleancode.exception.ResourceNotFoundException;
+import fr.esgi.cleancode.model.DrivingLicence;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
+
+import java.util.Optional;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatExceptionOfType;
+import static org.mockito.Mockito.when;
 
 @ExtendWith(MockitoExtension.class)
 class DrivingLicenceFinderServiceTest {
@@ -15,14 +23,20 @@ class DrivingLicenceFinderServiceTest {
 
     @Mock
     private InMemoryDatabase database;
+    @Mock
+    private DrivingLicenceIdGenerationService generationService;
 
     @Test
     void should_find() {
+        final var given = DrivingLicence.builder().id(generationService.generateNewDrivingLicenceId()).build();
+        when(database.findById(given.getId())).thenReturn(Optional.of(given));
 
+        final var actual = service.findById(given.getId()).orElse(null);
+        assertThat(actual).isEqualTo(given);
     }
 
     @Test
     void should_not_find() {
-
+        assertThat(service.findById(generationService.generateNewDrivingLicenceId()).isPresent()).isEqualTo(false);
     }
 }
